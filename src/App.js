8cf2909129f096f9e13e51b5ec29f1fff3a2d60d
Hoesung Jung <jrps2212@gmail.com>
2022-01-02 17:42:08 +0900

    vlpt 1-15. modification

--- src/App.js
+++ src/App.js
@@ -1,32 +1,80 @@
-import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
-import './App.css'
-import Counter from './Counter';
-import InputSample from './inputSample';
+import React, { useRef, useState } from 'react';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false
+    }
+  ]);
 
-  const name = 'react';
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    setUsers(users.concat(user));
 
-  const style = {
-    backgroundColor : 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding: '1rem'
-  }
+    setInputs({
+      username: '',
+      email: ''
+    });
+    nextId.current += 1;
+  };
+
+  const onRemove = id => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
+    setUsers(users.filter(user => user.id !== id));
+  };
+
+  const onToggle = id => {
+    setUsers(
+      users.map(user =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
+  };
 
   return (
     <>
-      <InputSample />
-      <Counter />
-      <Hello name = "app에서 hello로 넘겨주는 이름" color = "red" isSpecial/>
-
-      <Wrapper>
-        <Hello color="pink"/>
-        <div style={style}>{name}</div>
-        <div className="gray-box"></div>
-      </Wrapper>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
     </>
   );
 }
